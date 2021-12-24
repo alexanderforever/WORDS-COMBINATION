c80da0b3b1080e2732adf9946801c438358d1aea
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2021-12-24 13:54:08 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 import datetime
 import uuid
 from flask import Flask,render_template,url_for,request,redirect,jsonify,session
-from flask_login import login_user,LoginManager,current_user,logout_user
 from flask_sessionstore import Session
-from flask_wtf import Form ,CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from sqlalchemy.engine import Engine
 from sqlalchemy import event,create_engine,or_,desc
-from flask_bootstrap import Bootstrap  
 from flask_login import UserMixin
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_moment import Moment 
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='secrettttttttt'
 app.config['WTF_CSRF_SECRET_KEY']='wtffdjfk'
-
-login_manager=LoginManager()
-login_manager.init_app(app)
-
-Bootstrap(app)
 db = SQLAlchemy(app)
 csrf=CSRFProtect(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///combs.db'
